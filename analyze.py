80b955b5c1eff722e9cc7607043fde6f9bb34359
Tsolayemi Etchie <favouretchie07@gmail.com>
2026-09-22 17:08:17 +0100

    asking for username

---
+++ analyze.py
@@ -0,0 +1,9 @@
+print("\nWelcome to the text analysis tool, I will analyze a body of text you give me!")
+
+# Get Username
+
+# Print message prompting user to input user name
+username = input("\nTo begin, please enter your username\n")
+
+#Greet the user
+print("Hello, " + username)
