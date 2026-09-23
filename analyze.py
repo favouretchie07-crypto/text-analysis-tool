353160bb934484aa4ec9671a899521e3a28deafc
Tsolayemi Etchie <favouretchie07@gmail.com>
2026-09-23 11:02:44 +0100

    converted workflows into function

--- analyze.py
+++ analyze.py
@@ -1,9 +1,17 @@
-print("\nWelcome to the text analysis tool, I will analyze a body of text you give me!")
+def welcomeUser():
+    print("\nWelcome to the text analysis tool, I will analyze a body of text you give me!")
 
 # Get Username
-
-# Print message prompting user to input user name
-username = input("\nTo begin, please enter your username\n")
+def getUsername():
+    # Print message prompting user to input user name
+    usernameFromInput = input("\nTo begin, please enter your username\n")
+    return usernameFromInput
 
 #Greet the user
-print("Hello, " + username)
+def greetUser(name):
+print("Hello, " + name)
+welcomeUser()
+username = getUsername()
+greetUser(username)
+
+
---
+++ exercises.py
@@ -0,0 +1,8 @@
+count = 0
+
+def increment():
+    count = count + 1
+    print(count)
+
+increment(count)
+print("outside function", count)
